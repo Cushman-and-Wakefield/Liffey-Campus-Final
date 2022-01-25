357fa66891e60eb57917040baaf6d0c5f2d511fd
Cushman-and-Wakefield <97603362+Cushman-and-Wakefield@users.noreply.github.com>
2022-01-25 12:45:38 +0000

    Update barMaker.js

--- app/support/barMaker.js
+++ app/support/barMaker.js
@@ -37,7 +37,7 @@ define([
     applyRenderer, queryTools
 ) {
         return {
-
+           //For Area chart
             createChartData: function (selection, settings, bins) {
 
                 this.selection = selection;
@@ -213,6 +213,191 @@ define([
                 on(dom.byId("reload"), "click", function (event) {
 
                     settings.layer1.renderer = applyRenderer.createRendererVV(selection, settings.areaname);
+                    
+                    view.environment.lighting.directShadowsEnabled = true;
+                    view.environment.lighting.ambientOcclusionEnabled = true;
+                });
+
+            },
+         //For Lease Expiry Date
+            createChartData_exp: function (selection, settings, bins) {
+                
+                selection = new Date(selection);
+                selection = selection.getFullYear();
+                this.selection = selection;
+
+                var chartData = [];
+                var kernel = [];
+                var totalrange = [];
+                var years = [];
+
+                if (bins > selection.length) {
+                    bins = selection.length - 1;
+                }
+
+                for (var j = 0; j < selection.length; j++) {
+                    totalrange.push(selection[j].attributes[settings.leaseexpiryname]);
+                }
+
+                function maxIterate(arr) {
+                    var max = arr[0];
+                    for (var i = 0; i < arr.length; i++) {
+                        if (arr[i] > max) {
+                            max = arr[i];
+                        }
+                    }
+                    return max;
+                }
+
+                function minIterate(arr, max) {
+                    var min = max;
+                    for (var i = 0; i < arr.length; i++) {
+                        if (arr[i] < min) {
+                            min = arr[i];
+                        }
+                    }
+                    return min;
+                }
+
+                var max = maxIterate(totalrange);
+                var min = minIterate(totalrange, max);
+
+                if (Math.round(min) === 1) {
+                    min = 0;
+                }
+
+                var kernelwidth = (max - min) / bins;
+
+                if (kernelwidth > 1000) {
+                    kernelwidth = 500 * Math.round(kernelwidth / 500);
+                }
+                else if (kernelwidth < 1000 && kernelwidth > 500) {
+                    kernelwidth = 250 * Math.round(kernelwidth / 250);
+                }
+                else if (kernelwidth < 500 && kernelwidth > 200) {
+                    kernelwidth = 100 * Math.round(kernelwidth / 100);
+                }
+                else if (kernelwidth < 200 && kernelwidth > 100) {
+                    kernelwidth = 50 * Math.round(kernelwidth / 50);
+                }
+                else if (kernelwidth < 100 && kernelwidth > 50) {
+                    kernelwidth = 10 * Math.round(kernelwidth / 10);
+                }
+                else if (kernelwidth < 50 && kernelwidth > 10) {
+                    kernelwidth = 5 * Math.round(kernelwidth / 5);
+                }
+                else {
+                    kernelwidth = Math.round(kernelwidth);
+                }
+
+                min = kernelwidth * Math.round(min / kernelwidth);
+
+                var bins_new = (max - min) / kernelwidth;
+
+                // set up bins with ranges
+                for (var n = 0; n < bins_new; n++) {
+                    kernel.push({
+                        min: min,
+                        max: min + kernelwidth
+                    });
+                    min += kernelwidth;
+                }
+
+                var color = [];
+
+                if (bins_new > 9) {
+                    color = ["#E4002B", "#A6192B", "#9BD3DD", "#D9ECEB", "#0093B2", "#56AAC6", "#9EC8DB", "#003865", "#526180", "#001933"];
+
+                }
+                else {
+                    color = ["#E4002B", "#A6192B", "#9BD3DD", "#D9ECEB", "#0093B2", "#001933"];
+                }
+
+
+
+                for (var i = 0; i < kernel.length; i++) {
+                    chartData.push({
+                        kernel: Math.round(kernel[i].min) + "m2 - " + Math.round(kernel[i].max) + "m2",
+                        count: 0,
+                        subdata: [
+                            { min: kernel[i].min, max: kernel[i].max }
+                        ],
+                        "color": color[i]
+                    });
+                }
+
+                for (var k = 0; k < totalrange.length; k++) {
+                    for (var m = 0; m < kernel.length; m++) {
+                        if (totalrange[k] > kernel[m].min && totalrange[k] <= kernel[m].max) {
+                            chartData[m].count += 1;
+                        }
+                    }
+                }
+
+                return chartData;
+            },
+
+
+            createChart_exp: function (selection, data, settings, state, view, callback) {
+
+                var chart = AmCharts.makeChart("chartDiv", {
+                    "type": "serial",
+                    "theme": "light",
+                    "sequencedAnimation": false,
+                    "dataProvider": data,
+                    "fontSize": 12,
+                    "fontFamily": "Avenir LT W01 65 Medium",
+                    "valueAxes": [{
+                        "gridColor": "#FFFFFF",
+                        "gridAlpha": 0.2,
+                        "dashLength": 0
+                    }],
+                    "gridAboveGraphs": true,
+                    "startDuration": 1,
+                    "graphs": [{
+                        "balloonText": "[[category]]: <b>[[value]]</b>",
+                        "fillAlphas": 0.8,
+                        "lineAlpha": 0,
+                        "fillColorsField": "color",
+                        "type": "column",
+                        "valueField": "count"
+                    }],
+                    "chartCursor": {
+                        "categoryBalloonEnabled": false,
+                        "cursorAlpha": 0,
+                        "zoomable": false
+                    },
+                    "categoryField": "kernel",
+                    "categoryAxis": {
+                        "gridPosition": "start",
+                        "labelRotation": 45,
+                        "gridAlpha": 0,
+                        "tickPosition": "start",
+                        "tickLength": 20
+                    },
+                    "export": {
+                        "enabled": true
+                    }
+
+                });
+
+                callback("loaded");
+
+                chart.addListener("clickGraphItem", function (event) {
+
+                    var max = event.item.dataContext.subdata[0].max;
+                    var min = event.item.dataContext.subdata[0].min;
+                    var color = event.item.dataContext.color;
+
+                    settings.layer1.renderer = applyRenderer.createRendererVVbar(min, max, color, settings.leaseexpiryname);
+                    
+                    view.environment.lighting.directShadowsEnabled = false;
+                    view.environment.lighting.ambientOcclusionEnabled = false;
+                });
+
+                on(dom.byId("reload"), "click", function (event) {
+
+                    settings.layer1.renderer = applyRenderer.createRendererVV(selection, settings.leaseexpiryname);
                     
                     view.environment.lighting.directShadowsEnabled = true;
                     view.environment.lighting.ambientOcclusionEnabled = true;
