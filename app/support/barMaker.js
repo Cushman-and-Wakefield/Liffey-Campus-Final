7b4e0dc87b74a87c2a9da286e5dcab2945e65dbd
Cushman-and-Wakefield <97603362+Cushman-and-Wakefield@users.noreply.github.com>
2022-01-28 14:37:31 +0000

    Update barMaker.js

--- app/support/barMaker.js
+++ app/support/barMaker.js
@@ -402,10 +402,11 @@ define([
                 for (var k = 0; k < totalrange.length; k++) {
                     for (var m = 0; m < (unique_types.length)*2; m++) {
                          if ((totalrange[k] == 2024) && (totalreview[k] == unique_types[m])) {
-                              console.info(chartData[m].year);
+                              //console.info(chartData[m].year);
                               chartData[m].count += 1;
                          }
                          else if ((totalrange[k] == 2025) && (totalreview[k] == unique_types[m])) {
+                             console.info(chartData[m].year);
                              chartData[m].count += 1;
                          }
                     }
