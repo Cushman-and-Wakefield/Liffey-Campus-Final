e7686926ed2ea74da714a13e63129838f6be9acc
Cushman-and-Wakefield <97603362+Cushman-and-Wakefield@users.noreply.github.com>
2022-01-31 13:05:49 +0000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -135,7 +135,8 @@ define([
                     portalItem: {
                         id: this.settings.webscene
                     },
-                    basemap: "topo"
+                    basemap: "dark-gray-vector",
+                    ground: "world-elevation"
                 });
 
                 // create a view
