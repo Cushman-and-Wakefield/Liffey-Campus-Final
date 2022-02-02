dc92ba1aa17d8f4feb87772cf1c4f22933d48b84
Cushman-and-Wakefield <97603362+Cushman-and-Wakefield@users.noreply.github.com>
2022-02-02 12:33:25 +0000

    Update chartMaker.js

--- app/support/chartMaker.js
+++ app/support/chartMaker.js
@@ -112,10 +112,91 @@ define([
                     "colorField": "color",
                     "groupPercent": groupPercentValue,
                     "startRadius": 100,
+                    "fontSize": 12,
+                    "fontFamily": "Avenir LT W01 65 Medium",
+                    "radius": 100,
+                    "marginTop": 15,
+                    "pieAlpha": 0.8,
+                    "sequencedAnimation": true,
+                    "balloon": {
+                        "fixedPosition": true
+                    },
+                    "clickSlice": function (dataItem, event) {
+
+                        var value = dataItem.title;
+
+                        var fields = [];
+                        for (var i = 0; i < settings.values.length; i++) {
+                            fields.push({
+                                values: settings.values[i],
+                                color: [135, 135, 135, 0.2]
+                            });
+                        }
+
+                        for (var j = 0; j < fields.length; j++) {
+                            if (fields[j].values === value) {
+                                fields[j].color = color[j];
+                            }
+                        }
+
+                        var selectedvalues = [];
+                        var selectedcolor = [];
+
+                        for (var k = 0; k < fields.length; k++) {
+                            selectedvalues.push(fields[k].values);
+                            selectedcolor.push(fields[k].color);
+                        }
+
+
+                        if (dataItem.pulled) {
+                            chart.pullSlice(dataItem, 0);
+ 
+                            settings.layer1.renderer = applyRenderer.createRenderer(settings.values, settings.color, settings.usagename);
+                            
+                            view.environment.lighting.directShadowsEnabled = true;
+                            view.environment.lighting.ambientOcclusionEnabled = true;
+
+                        } else {
+                            chart.pullSlice(dataItem, 1);
+
+                            settings.layer1.renderer = applyRenderer.createRenderer(selectedvalues, selectedcolor, settings.usagename);
+                            
+                            view.environment.lighting.directShadowsEnabled = false;
+                            view.environment.lighting.ambientOcclusionEnabled = false;
+                        }
+                    }.bind(this),
+                    "export": {
+                        "enabled": true
+                    }
+                });
+                callback("loaded");
+
+            },
+         
+           createChart_small: function (view, data, settings, state, callback) {
+
+                var color = this.rgbToHex(settings.color);
+
+                var groupPercentValue = 0;
+
+                if (settings.name !== "Zurich") {
+                    groupPercentValue = "0";
+                }
+
+                var chart = AmCharts.makeChart("chartDiv", {
+                    "type": "pie",
+                    "theme": "light",
+                    "hideCredits":true,
+                    "dataProvider": data,
+                    "valueField": "area",
+                    "titleField": "usage",
+                    "colorField": "color",
+                    "groupPercent": groupPercentValue,
+                    "startRadius": 100,
                     "fontSize": 10,
                     "fontFamily": "Avenir LT W01 65 Medium",
-                    "radius": 90,
-                    "marginTop": 15,
+                    "radius": 50,
+                    "marginTop": 10,
                     "pieAlpha": 0.8,
                     "sequencedAnimation": true,
                     "balloon": {
