2888885dfedba8ea08185b74a5169cddc5975c54
Cushman-and-Wakefield <97603362+Cushman-and-Wakefield@users.noreply.github.com>
2022-02-02 12:40:13 +0000

    Update VizTool.js

--- app/VizTool.js
+++ app/VizTool.js
@@ -332,10 +332,24 @@ define([
 
                     domStyle.set(dom.byId("chartDiv"), { "opacity": 1 });
                     domStyle.set(dom.byId("statsDiv"), { "opacity": 0 });
+                 
+                    function checkMediaQuery() {
+                      // If the inner width of the window is greater then 768px
+                      if (window.innerWidth > 1280) {
+                        // Then log this message to the console
+                        chartMaker.createChart(this.view, initCharts.usage, settings, "city", function (state) {
+                             this.menu.setLoadingState("loaded");
+                        }.bind(this));
+                      }
+                      else {
+                        chartMaker.createChart_small(this.view, initCharts.usage, settings, "city", function (state) {
+                             this.menu.setLoadingState("loaded");
+                        }.bind(this));
+                      }
+                    }
 
-                    chartMaker.createChart(this.view, initCharts.usage, settings, "city", function (state) {
-                        this.menu.setLoadingState("loaded");
-                    }.bind(this));
+                    // Add a listener for when the window resizes
+                    window.addEventListener('resize', checkMediaQuery);
                 }
                 if (vizName === "tenancy") {
                     settings.layer1.renderer = applyRenderer.createRenderer(settings.values_ten, settings.color, settings.tenancyname);
@@ -410,9 +424,25 @@ define([
                         domStyle.set(dom.byId("statsDiv"), { "opacity": 0 });
 
                         var chartData = chartMaker.createChartData(selection, settings);
-                        chartMaker.createChart(view, chartData, settings, "building", function (state) {
+                        function checkMediaQuery() {
+                          // If the inner width of the window is greater then 768px
+                          if (window.innerWidth > 1280) {
+                            // Then log this message to the console
+                            chartMaker.createChart(view, chartData, settings, "building", function (state) {
                             menu.setLoadingState(state);
                         });
+                          }
+                          else {
+                            chartMaker.createChart_small(view, chartData, settings, "building", function (state) {
+                            menu.setLoadingState(state);
+                        });
+                          }
+                        }
+
+                        // Add a listener for when the window resizes
+                        window.addEventListener('resize', checkMediaQuery);   
+                     
+                     
 
                         var data = statsMaker.createChartData(selection, settings);
                         statsMaker.createChart(data, function (state) {
