a22c47ec5632308e230943071fce05aee3a01dfe
Cushman-and-Wakefield <97603362+Cushman-and-Wakefield@users.noreply.github.com>
2022-02-02 14:56:53 +0000

    Update VizTool.js

--- app/VizTool.js
+++ app/VizTool.js
@@ -352,7 +352,7 @@ define([
                  var chart = checkMediaQuery();
                  console.info(chart);
                  
-                 window["chart"](this.view, initCharts.usage, settings, "city", function (state) {
+                 this[chart](this.view, initCharts.usage, settings, "city", function (state) {
                          this.menu.setLoadingState("loaded");
                     }.bind(this));
 
@@ -453,7 +453,7 @@ define([
                          var chart = checkMediaQuery();
                          console.info(chart);
                      
-                         window["chart"](view, chartData, settings, "building", function (state) {
+                         this[chart](view, chartData, settings, "building", function (state) {
                             menu.setLoadingState(state);
                             });
                      
