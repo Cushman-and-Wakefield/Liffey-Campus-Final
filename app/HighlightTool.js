6392bee8fc4de4742d89a5151fa90fc74efa1084
Cushman-and-Wakefield <97603362+Cushman-and-Wakefield@users.noreply.github.com>
2022-02-03 17:01:11 +0000

    Update HighlightTool.js

--- app/HighlightTool.js
+++ app/HighlightTool.js
@@ -1,21 +1,12 @@
  /* Copyright 2017 Esri
-
    Licensed under the Apache License, Version 2.0 (the "License");
-
    you may not use this file except in compliance with the License.
-
    You may obtain a copy of the License at
-
        http://www.apache.org/licenses/LICENSE-2.0
-
    Unless required by applicable law or agreed to in writing, software
-
    distributed under the License is distributed on an "AS IS" BASIS,
-
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
    See the License for the specific language governing permissions and
-
    limitations under the License.
    ​
    */
@@ -32,6 +23,7 @@
  */
 
 define([
+    "esri/core/Accessor",
     "esri/tasks/support/Query",
 
     "dojo/dom-construct",
@@ -52,7 +44,6 @@ define([
             declaredClass: "c-through.HighlightTool",
 
             constructor: function (params) {
-
 
                 this.container = params.container;
                 this.menu = params.menu;
